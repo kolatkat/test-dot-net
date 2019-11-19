f251363d05f702db99d7d7bfdc36bc11169f4dec
Kasia Kołata <katarzyna.kolata3@gmail.com>
2019-11-19 19:35:13 +0100

    Pierwsza wersja drugiej aplikacji

---
+++ DrugaAplikacjaKonsolowa/DrugaAplikacjaKonsolowa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugaAplikacjaKonsolowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Naciśnij dowolny klawisz aby zamknac aplikacje...");
+            Console.ReadKey();
+        }
+    }
+}
